8b5536d6176423fb80f0297caeafc10b367ac99b
Benjamin <ayzenbergb@gmail.com>
2017-04-26 11:31:21 -0700

    working code

--- lab-ben/server.js
+++ lab-ben/server.js
@@ -15,13 +15,20 @@ const server = module.exports = http.createServer(function(req, res) {
     if(req.url.pathname === '/cowsay') {
       bodyParser(req, function(err) {
         if(err) console.error(err);
-        let message = cowsay.say({text: req.body.text});
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(message);
-        res.end();
+        if (req.body.text) {
+          let message = cowsay.say({text: req.body.text});
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.write(message);
+          res.end();
+        } else {
+          let message = cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'});
+          res.writeHead(400, {'Content-Type': 'text/plain'});
+          res.write(message);
+          res.end();
+        }
       });
     } else {
-      let message = cowsay.say({text: 'bad request\ntry localhost:3000/cowsay with a proper body'});
+      let message = cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'});
       res.writeHead(400, {'Content-Type': 'text/plain'});
       res.write(message);
       res.end();
@@ -29,9 +36,30 @@ const server = module.exports = http.createServer(function(req, res) {
   }
 
   if(req.method === 'GET') {
-
+    if(req.url.pathname === '/cowsay') {
+      if(req.url.query.text) {
+        let message = cowsay.say({text: req.url.query.text});
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(message);
+        res.end();
+      } else {
+        let message = cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'});
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write(message);
+        res.end();
+      }
+    } else if(req.url.pathname === '/') {
+      let message = cowsay.say({text: 'hello world'});
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(message);
+      res.end();
+    } else {
+      let message = cowsay.say({text: 'bad request\ntry localhost:3000/cowsay?text=howdy'});
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write(message);
+      res.end();
+    }
   }
-
 });
 
 server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- lab-ben/test/server-test.js
+++ lab-ben/test/server-test.js
@@ -14,47 +14,75 @@ describe('Server module', function() {
     done();
   });
 
-  describe('POST method', function() {
+  describe('#POST method', function() {
     describe('/ endpoint', function() {
       it('should respond with a 400 on bad request', done => {
-        chai.requuest(server)
-        .post('/monkeysay')
+        chai.request(server)
+        .post('/')
         .send({})
         .end((err, res) => {
+          expect(res.text).to.include('bad request');
           expect(res.status).equal(400);
+          done();
         });
-        done();
       });
     });
 
     describe('/cowsay endpoint', function() {
       it('should respond with a 200 on proper request', done => {
-
-        done();
+        chai.request(server)
+        .post('/cowsay')
+        .send({'text': 'hello world'})
+        .end((err, res) => {
+          expect(res.text).include('hello world');
+          expect(res).to.be.status(200);
+          done();
+        });
       });
       it('should respond with a 400 on bad request', done => {
-
-        done();
+        chai.request(server)
+        .post('/cowsay')
+        .send({})
+        .end((err, res) => {
+          expect(res.text).to.include('bad request');
+          expect(res).to.be.status(400);
+          done();
+        });
       });
     });
   });
 
-  describe('GET method', function() {
+  describe('#GET method', function() {
     describe('/ endpoint', function() {
-      it('should respond with a 400 on bad request', done => {
-
-        done();
+      it('should respond with a 200 on proper request', done => {
+        chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(res.text).include('hello world');
+          expect(res).to.be.status(200);
+          done();
+        });
       });
     });
 
     describe('/cowsay endpoint', function() {
       it('should respond with a 200 on proper request', done => {
-
-        done();
+        chai.request(server)
+        .get('/cowsay?text=hello')
+        .end((err, res) => {
+          expect(res.text).to.include('hello');
+          expect(res).to.be.status(200);
+          done();
+        });
       });
       it('should respond with a 400 on bad request', done => {
-
-        done();
+        chai.request(server)
+        .get('/cowsay')
+        .end((err, res) => {
+          expect(res.text).to.include('bad request');
+          expect(res).to.be.status(400);
+          done();
+        });
       });
     });
   });
