0708fe21d631595bc68d0a573954a73fa8c87a18
Michael Padget <mpadget@comcast.net>
2017-04-26 11:28:14 -0700

    Working GET query text

--- lab-padget/server.js
+++ lab-padget/server.js
@@ -15,14 +15,16 @@ const server = module.exports = http.createServer(function(req, res) {
   if(req.method === 'POST') {
     if(req.url.pathname === '/cowsay') {
       return bodyParser(req, function(err) {
+      // bodyParser(req, function(err) {
         if(err) console.error(err);
         let message = cowsay.say({text: req.body.text});
-        console.log(req.body);
+        console.log('Post request body ', req.body);
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write(message);
         res.end(); // this will not be here; will be in a asych callback(s)
       });
     } else {
+      // a body including the value returned from cowsay.say
       let message = cowsay.say(
         {text: 'bad request\ntry localhost:3000/cowsay with a proper body'}
       );
@@ -33,10 +35,56 @@ const server = module.exports = http.createServer(function(req, res) {
   }
 
   if(req.method === 'GET') {
-    console.log('Log of req.method GET');
+    if(req.url.pathname === '/cowsay') {
+      // return bodyParser(req, function(err) {
+
+      console.log('Get request body ', req.url.query.text);
+
+      // http GET :3000/cowsay?text=helloworld
+      let message = cowsay.say({text: req.url.query.text});
+
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(message);
+      res.end(); // this will not be here; will be in a asych callback(s)
+    } else {
+      // a body including the value returned from cowsay.say
+      let message = cowsay.say(
+        {text: 'bad request\ntry localhost:3000/cowsay with a proper body'}
+      );
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write(message);
+      res.end();
+    }
   }
-
-
 });
 
 server.listen(PORT, () => console.log(`Listening on port, ${PORT}`));
+
+// For cowsay route.
+// if(req.url.pathname === '/cowsay') {
+//   //return bodyParser(req, function(err) {
+//
+//   // Hold text from user input.
+//   // let queryText = req.body.text;
+//   // let queryText = req.url.query.text;
+//   // let message = req.body.text;
+//   // console.log('Get log: ', req.url.query.text);
+//   // // If no text from user.
+//   // if (!req.url.query.text) {
+//   //
+//   //   // Throw error message.
+//   //   let message = cowsay.say(
+//   //     {text: 'bad request\ntry localhost:3000/cowsay with a proper body'}
+//   //   );
+//   //
+//   //   // Error code bad request.
+//   //   res.writeHead(400, {'Content-Type': 'text/plain'});
+//   //   res.write(message);
+//   //   res.end();
+//   // }
+//
+//   // Otherwise show text from user, status 200 ok.
+//   // res.writeHead(200, {'Content-Type': 'text/plain'});
+//   // res.write(cowsay.say({text: req.url.query.text}));
+//   // res.end();
+// }
