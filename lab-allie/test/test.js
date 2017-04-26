5a5486276bca9ae635ff44b90c136f661ab353ca
Allie Grampa <agrampa@yahoo.com>
2017-04-25 18:56:08 -0700

    tests are scaffolded

---
+++ lab-allie/test/test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const server = require('../server.js');
+const cowsay = require('cowsay');
+const chai = require('chai');
+const http = require('chai-http');
+const expect = chai.expect;
+
+chai.use(http);
+
+describe('Server module', function() {
+  before(done => {
+    server.listen(3000);
+    done();
+  });
+  
+  describe('POST method', function() {
+    describe('/ endpoint', function() {
+      it('should respond with a 404 on bad request', done => {
+        chai.request(server)
+        .post('/blah')
+        .send({})
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+        });
+        done();
+      });
+    });
+    
+    describe('/cowsay endpoint', function() {
+      it('should respond with a 200 on proper request', done => {
+        
+        
+        
+        done();
+      });
+      
+      it('should respond with a 400 on bad request', done => {
+        
+        
+        
+        done();
+      });
+    });
+  });
+  
+  describe('GET meethod', function() {
+    describe('/ endpoint', function() {
+      it('should respond with a 400 on bad request', done => {
+        
+        
+        
+        
+        done();
+      });
+    });
+    
+    describe('/cowsay endpoint', function() {
+      it('should respond with a 200 on proper request', done => {
+        
+        
+        done();
+      });
+      
+      it('should respond with a 400 on bad request', done => {
+        
+        
+        
+        done();
+      });
+    });
+  });
+  
+  after(done => {
+    server.close();
+    done();
+  });
+});
+
